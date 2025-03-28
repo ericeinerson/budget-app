d880408cae35beaf5198651ef20e31526aab3a2e
Eric Einerson <eaeiner.gmail.co>
2025-03-28 17:13:13 -0600

    extract backing class

--- Pages/Archive.razor.cs
+++ Pages/Archive.razor.cs
@@ -1,1 +1,13 @@
+namespace budget_app.Pages;
 
+public partial class Archive
+{
+    private BudgetItem[]? BudgetItems { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        using var context = ContextFactory.CreateDbContext();
+
+        BudgetItems = await context.BudgetItems.ToArrayAsync();
+    }
+}
